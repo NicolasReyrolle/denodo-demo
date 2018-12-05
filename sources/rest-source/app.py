892e6fc1219dcb265ac08265b535807ae172e27e
NicolasReyrolle <405928+NicolasReyrolle@users.noreply.github.com>
2018-12-05 16:42:49 +0100

    Removed useless parenthesis

--- sources/rest-source/app.py
+++ sources/rest-source/app.py
@@ -26,7 +26,7 @@ users = [
 class User(Resource):
     def get(self, name):
         for user in users:
-            if (name == user["name"]):
+            if name == user["name"]:
                 return user, 200
         return "User not found", 404
 
